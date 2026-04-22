ef60a3a3e81d0fcee003860333bc6466a92a5994
Muhammad Ahmad <m.ahmadmalik03@gmail.com>
2026-04-23 00:32:47 +0500

    Updated URLS

--- shortner/urls.py
+++ shortner/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'shortner'
+
 urlpatterns = [
-    
+    path('', views.create, name='create'),
+    path('r/<str:code>/', views.redirect_url, name='redirect'),
 ]
